b1959fc75ef6b715b96f1fd8e4855d93d562b511
Mitra A <mitra091202@gmail.com>
2025-03-13 14:21:29 +0800

    [Fix] mismatch title

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-<title>Profil Desa</title>
+<title>SMK Penerbangan</title>
 <meta name="description" content="" />
 
 <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
